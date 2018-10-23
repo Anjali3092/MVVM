e589e5c90675541ae1379fe7a9c1c11c90d1e9c6
Anjali <>
2018-10-23 20:37:34 +0530

    First commit

---
+++ MVVM_Architecture/DogViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DogViewModel.swift
+//  MVVM_Architecture
+//
+//  Created by Anjali on 22/10/18.
+//  Copyright © 2018 Anjali. All rights reserved.
+//
+
+import Foundation
---
+++ MVVM_Architecture/MVCViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_Architecture
+//
+//  Created by Anjali on 22/10/18.
+//  Copyright © 2018 Anjali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
